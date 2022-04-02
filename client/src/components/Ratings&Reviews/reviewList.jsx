1322e7c2626c218dce2677c4871796cf8f73eef8
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-02 14:16:35 -0700

    added API and textboxes

--- client/src/components/Ratings&Reviews/reviewList.jsx
+++ client/src/components/Ratings&Reviews/reviewList.jsx
@@ -1,19 +1,44 @@
 import React from "react";
 import ReviewListView from "./reviewListView.jsx";
+import axios from "axios";
+import config from "../../../../config.js";
+import DefaultReviews from "./DefaultReviews.js";
+
+const host = ' https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+const headers = {
+  'Authorization' : `${config.TOKEN}`
+};
 
 class ReviewList extends React.Component{
+    
     constructor(props) {
         super(props);
         this.state = {
-
+            productID : 64620,
+            currentReview : DefaultReviews
         }
     }
+    componentDidMount() {
+        // this.getReview();
+        console.log(this.state.currentReview, 'reviessssss')
+    }
+
+    // getReview() {
+    //     axios.get(`${host}/reviews?count=5&sort='newest'&product_id=${this.state.productID}`, {headers})
+    //     .then((output)=> {
+    //         this.setState({
+    //             currentReview : output.data.result
+    //         })
+    //         console.log(output.data.results)
+    //     })
+    //     .catch(err => console.log(err));
+    // } 
 
     render() {
         return (
             <div> 
                 <p> # of reviews sort by "  " </p>
-                <ReviewListView /> 
+                <ReviewListView reviews={this.state.currentReview}/> 
             </div>
         )
     }
--- client/src/components/Ratings&Reviews/reviewListView.jsx
+++ client/src/components/Ratings&Reviews/reviewListView.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const ReviewListView = ()=> {
+const ReviewListView = ({reviews})=> {
     return (
         <div>
             stars + user/postDate
             <div> </div>
-            <div> Reviews Title </div>
-            <div> Reviews Content</div>
+            {reviews.map((review,index) => {
+                return (<div className="individualReview_container" key={index}>
+                    <div className="reviewSummary"> {review.summary}</div>
+                    <div className="reviewBody"> {review.body}</div>
+                    </div>
+                )
+            })}
             </div>
     )
 }
