e1d43eb347a2b5c1efe44ac3cefd0194bfa2bb33
Nicole Qi <nicoleqi@nicoledembp.lan>
2022-04-03 23:14:00 -0700

    render all questions and answers with show more answers button

--- client/src/components/Q&A/QuestionsList/QuestionCard_app.jsx
+++ client/src/components/Q&A/QuestionsList/QuestionCard_app.jsx
@@ -1,21 +1,29 @@
-import React, {Component} from 'react';
-import OneQuestion from './OneQuestion.jsx';
-import Answers from './Answers.jsx';
+import React, { Component } from "react";
 
+import Answers from "./Answers.jsx";
 
-class QuestionCard extends React.Component{
+class QuestionCard extends React.Component {
   constructor(props) {
-      super(props);
-      console.log(props);
+    super(props);
+    console.log(props.answers);
   }
 
   render() {
-      return (
-          <div>
+    const {question_body, answers} = this.props;
 
-          QuestionCard
-          </div>
-          )
+    const onSeeMoreAnswersClick = () => {
+    }
+
+    return (
+      <div>
+        <h3>{question_body}</h3>
+        <Answers answersArray={answers} ></Answers>
+        {Object.keys(answers).length > 2
+          ? <button onClick={onSeeMoreAnswersClick}>See more answers</button>
+          : <></>
+        }
+      </div>
+    );
   }
 }
 
