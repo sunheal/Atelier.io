55e2b5ca9c4f3c9d2718202a30c9b3274a5e39a2
Nicole Qi <nicoleqi@nicoledembp.lan>
2022-04-07 11:02:51 -0700

    04/07/22 backup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
-var path = require('path');
+var path = require("path");
 var SRC_DIR = path.join(__dirname, "client/src");
 var DIST_DIR = path.join(__dirname, "client/dist");
 
 module.exports = {
-    entry: `${SRC_DIR}/index.js`,
-    output: {
-      filename: "bundle.js",
-      path: DIST_DIR,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-              }
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-        }],
-    },
-  };
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
