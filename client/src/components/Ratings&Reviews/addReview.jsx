e5e1b3a39c4c070579f5600dbc30806cee6515ce
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-07 10:18:19 -0700

    fixed modal and z-index on Stars

--- client/src/components/Ratings&Reviews/addReview.jsx
+++ client/src/components/Ratings&Reviews/addReview.jsx
@@ -8,16 +8,65 @@ class AddReview extends React.Component {
 
     render() {
         return (
-            <Modal className="reviewModal" isOpen={this.props.show} ariaHideApp={false}>
+            <div className="reviewModal"> 
+             <Modal className='innerModal' isOpen={this.props.show} ariaHideApp={false}>
                 <h1> Hello </h1>
                 <form> 
                     <label> Name: 
                     <input type= 'text' />
                     </label>
-                   
+                    <br></br>
+                    <label> Do you recommend this product?: 
+                        <input name= 'recommend' type= "radio" /> Yes
+                        <input name='recommend' type="radio" /> No
+                    </label>
+                    <br></br>
+                    <label> Review Summary: 
+                        <textarea rows='4' cols='50' maxLength='60' />
+                    </label>
+                    <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>
+
+                    <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>
+                    <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>  <br></br>
+                    <label> Review Body: 
+                    <textarea rows='4' cols='50' maxLength='1000' />
+                    </label>
+
                 </form>
                 <button onClick={this.props.onShowModal}> Close </button>
             </Modal>
+            </div>
+           
             )
     }
 }
