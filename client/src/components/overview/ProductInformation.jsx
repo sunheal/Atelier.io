a0bc7329a1b488f654f6e66668b9219f7a3d4231
sunheal <sunheal@gmail.com>
2022-04-06 21:49:56 -0400

    WIP ProductInformation

--- client/src/components/overview/ProductInformation.jsx
+++ client/src/components/overview/ProductInformation.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 const ProductInformation = (props) => {
   return (
-    <div>ProductInformation</div>
+    <div>
+      <h4>ProductInformation</h4>
+      <div>Ratings: {props.product_ratings}</div>
+      <div>Read All {props.reviewsCount} Reviews</div>
+      <div>Category: {props.information.category}</div>
+      <div>Title: {props.information.name}</div>
+      <div>Price: {props.information.default_price}</div>
+      <div>Product Overview: {props.information.description}</div>
+    </div>
   );
 }
 
