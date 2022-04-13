fdab9eb846ca2b4323136dbe7d9fb6c178d9c65e
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-12 18:18:44 -0700

    added ratingBar div on RR

--- client/src/components/Ratings&Reviews/breakdowns.jsx
+++ client/src/components/Ratings&Reviews/breakdowns.jsx
@@ -10,10 +10,35 @@ import React from "react"
   
     render() {
         return (
-            <svg> 
-
-                <rect width='100' height='140'/>
+            <div> 
+            <svg height='30' width="300"> 
+                <text className="name-label" x="0" y="15">5 Stars</text>
+                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                <rect className='green' width='60' height='10'  x='90' y='5' fill="green" />
             </svg>
+            <svg height='30' width="300"> 
+                <text className="name-label" x="0" y="15">4 Stars</text>
+                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                <rect className='green' width='60' height='10'  x='90' y='5' fill="green"/>
+            </svg>
+            <svg height='30' width="300"> 
+                <text className="name-label" x="0" y="15">3 Stars</text>
+                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                <rect className='green' width='60' height='10'  x='90' y='5' fill="green"/>
+            </svg>
+            <svg height='30' width="300"> 
+                <text className="name-label" x="0" y="15">2 Stars</text> 
+                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                <rect className='green' width='60' height='10'  x='90' y='5' fill="green"/>
+            </svg>
+            <svg height='30' width="300"> 
+                <text className="name-label" x="0" y="15">1 Stars</text>
+                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                <rect className='green' width='60' height='10'  x='90' y='5' fill="green"/>
+            </svg>
+
+            </div>
+            
         )
     }
 
