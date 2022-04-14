122fa9add9514e187f0ad3c32885776b3d6b773c
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-13 17:21:35 -0700

    figured out how to add product breakdown and refactored rating bar

--- client/src/components/Ratings&Reviews/breakdowns.jsx
+++ client/src/components/Ratings&Reviews/breakdowns.jsx
@@ -12,9 +12,25 @@ import React from "react"
 
 
     render() {
+        const aa = 30;
         return (
             <div> 
-            <svg height='30' width="300"> 
+                <div className="starBreakDown_RR">
+                {[...Array(5)].map((bar, index)=>{
+                    const order = 5 - (index);
+                    return (
+                        <svg height='30' width="300" key={order}> 
+                            <text className="name-label" x="0" y="15">{order} Stars</text>
+                            <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
+                            <rect className='green' width={((this.props.ratings[order]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green" />
+                        </svg>
+                    )
+                })}
+                </div>
+                <svg width='100' height='10'> 
+                <polygon points="0,0 10,0 5,10" transform={`translate(${aa} 0)`}/>
+                </svg>
+            {/* <svg height='30' width="300"> 
                 <text className="name-label" x="0" y="15">5 Stars</text>
                 <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
                 <rect className='green' width={((this.props.ratings[5]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green" />
@@ -38,7 +54,7 @@ import React from "react"
                 <text className="name-label" x="0" y="15">1 Stars</text>
                 <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
                 <rect className='green' width={((this.props.ratings[1]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green"/>
-            </svg>
+            </svg> */}
 
             </div>
             
