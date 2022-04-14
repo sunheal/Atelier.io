0bb2b535ee855ae11ddccb2e08dca755b5c474e3
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-13 19:14:10 -0700

    fixed reviews count issues

--- client/src/components/Ratings&Reviews/breakdowns.jsx
+++ client/src/components/Ratings&Reviews/breakdowns.jsx
@@ -30,31 +30,7 @@ import React from "react"
                 <svg width='100' height='10'> 
                 <polygon points="0,0 10,0 5,10" transform={`translate(${aa} 0)`}/>
                 </svg>
-            {/* <svg height='30' width="300"> 
-                <text className="name-label" x="0" y="15">5 Stars</text>
-                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
-                <rect className='green' width={((this.props.ratings[5]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green" />
-            </svg>
-            <svg height='30' width="300"> 
-                <text className="name-label" x="0" y="15">4 Stars</text>
-                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
-                <rect className='green' width={((this.props.ratings[4]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green"/>
-            </svg>
-            <svg height='30' width="300"> 
-                <text className="name-label" x="0" y="15">3 Stars</text>
-                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
-                <rect className='green' width={((this.props.ratings[3]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green"/>
-            </svg>
-            <svg height='30' width="300"> 
-                <text className="name-label" x="0" y="15">2 Stars</text> 
-                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
-                <rect className='green' width={((this.props.ratings[2]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green"/>
-            </svg>
-            <svg height='30' width="300"> 
-                <text className="name-label" x="0" y="15">1 Stars</text>
-                <rect className='grey' width='100' height='10' x='90' y='5'fill="grey" opacity='0.25'/>
-                <rect className='green' width={((this.props.ratings[1]/this.props.count)*100).toString()} height='10'  x='90' y='5' fill="green"/>
-            </svg> */}
+          
 
             </div>
             
--- client/src/components/Ratings&Reviews/reviewList.jsx
+++ client/src/components/Ratings&Reviews/reviewList.jsx
@@ -38,9 +38,9 @@ class ReviewList extends React.Component{
         })
     }
     getReviewinfo() {
-        axios.get(`/reviews/?product_id=${this.props.id}`)
+        axios.get(`/reviews/?product_id=${this.props.id}&count=5000`)
         .then((output)=> {
-            console.log(output.data.results, 'received from API');
+            console.log(output.data.results.length, 'received from API');
             this.setState({
                 currentReview : output.data.results,
                 whatShowing: output.data.results.slice(0,2)
