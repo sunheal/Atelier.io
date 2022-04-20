50cd720b475717bb3556156c139e5074e9d0f2f3
sunheal <sunheal@gmail.com>
2022-04-19 21:09:06 -0400

    Added Shared Stars Component to Product Information

--- client/src/components/overview/ProductInformation.jsx
+++ client/src/components/overview/ProductInformation.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Star from '../Shared/Stars.jsx'
 
 const ProductInformation = (props) => {
   return (
     <div>
       <h4>ProductInformation</h4>
-      <div>Ratings: {props.ratings}</div>
+      <div>Ratings: <Star rating={props.ratings}/></div>
       <div>Read All {props.reviewsCount} Reviews</div>
       <div>Category: {props.information.category}</div>
       <div>Title: {props.information.name}</div>
