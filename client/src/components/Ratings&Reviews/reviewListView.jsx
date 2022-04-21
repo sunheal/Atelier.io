9e9af7387ba9f3fdf6e0bde46006f7e75dde0148
“Kylekk812” <kang2015kyle@gmail.com>
2022-04-20 19:02:17 -0700

    fixed issues

--- client/src/components/Ratings&Reviews/reviewListView.jsx
+++ client/src/components/Ratings&Reviews/reviewListView.jsx
@@ -2,40 +2,40 @@ import React from "react";
 import Stars from "../Shared/Stars.jsx";
 import HelpfulAndReport from "./Helpful&Report.jsx";
 
-const ReviewListView =({reviews}) => {
+const ReviewListView = ({ reviews }) => {
     return (
         <div className="bigReview_container">
-            {reviews.map((review,index) => {
+            {reviews.map((review, index) => {
                 return (
-                <div className="individualReview_container" key={index}>
-                    <div className="starContainer" key={index}>
-                        <div className="indiStar"> <Stars rating={review.rating} /> </div> 
-                        <div className="userAndPostdate"> {review.reviewer_name}, {review.date.slice(0,10)}</div>
-                        <div className="spaceBetweenRev" key={index}> </div>
-                    </div>
-            <div className="reviewSpace" key={index+10}> </div>
-                    <div className="reviewSummary"> {review.summary}</div>
-                    <div className="spaceBetweenRev" key={index+1}> </div>
-                    <div className="reviewBody"> {review.body}</div>
-                    <div className="spaceBetweenRev" key={index+2}> </div>
-                    <div className="reviewPhotos"> {review.photos.map(photo => {
-                        return (
-                            <div className="RR_photos" key={index}> 
-                        <img src={photo.url} width='150' height='150'/> &nbsp;
-                            </div>
-                        )
+                    <div className="individualReview_container" key={index}>
+                        <div className="starContainer" >
+                            <div className="indiStar"> <Stars rating={review.rating} /> </div>
+                            <div className="userAndPostdate"> {review.reviewer_name}, {review.date.slice(0, 10)}</div>
+                            <div className="spaceBetweenRev" > </div>
+                        </div>
+                        <div className="reviewSpace" > </div>
+                        <div className="reviewSummary"> {review.summary}</div>
+                        <div className="spaceBetweenRev" > </div>
+                        <div className="reviewBody"> {review.body}</div>
+                        <div className="spaceBetweenRev" > </div>
+                        <div className="reviewPhotos"> {review.photos.map((photo,index) => {
+                            return (
+                                <div className="RR_photos" key={index}>
+                                    <img src={photo.url} width='150' height='150' /> &nbsp;
+                                </div>
+                            )
                         })
-                    }</div>
-                    {review.recommend ? <span className="checkmark"> ✓ I recommend this product </span> : null}
-                    <div className="spaceBetweenRev" key={index+3}> </div>
-                    {review.response ? <span className="response_RR"> {review.response} </span> : null}
-                    <div className="spaceBetweenRev" key={index+3}> </div>
-                    <HelpfulAndReport review={review} />
-                <div className="spaceBetweenRev" key={index+4}> </div>
-                </div>
+                        }</div>
+                        {review.recommend ? <span className="checkmark"> ✓ I recommend this product </span> : null}
+                        <div className="spaceBetweenRev" > </div>
+                        {review.response ? <span className="response_RR"> {review.response} </span> : null}
+                        <div className="spaceBetweenRev" > </div>
+                        <HelpfulAndReport review={review} />
+                        <div className="spaceBetweenRev" > </div>
+                    </div>
                 )
             })}
-            </div>
+        </div>
     )
 }
 export default ReviewListView;
