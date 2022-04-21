42552b2d23a3c12c61b00566648799f7e0171a19
Nicole <79296688+nicolel2021@users.noreply.github.com>
2022-04-20 18:36:01 -0700

    wip - mini carousel in preview image

--- client/src/components/relatedProduct/ProductCard/PreviewImages.jsx
+++ client/src/components/relatedProduct/ProductCard/PreviewImages.jsx
@@ -6,23 +6,27 @@ class PreviewImages extends React.Component {
     this.state = {
       current: 0,
       length: 4,
-      // images: []
+      image: {}
     };
     this.prevSlide = this.prevSlide.bind(this);
     this.nextSlide = this.nextSlide.bind(this);
   }
 
   prevSlide() {
-    const {current, length} = this.state;
+    const { current, length } = this.state;
     if (current !== 0) {
       this.setState({
         current: current - 1
+      })
+    } else {
+      this.setState({
+        length: length - 1
       })
     }
   }
 
   nextSlide() {
-    const {current, length} = this.state;
+    const { current, length } = this.state;
     if (current !== length - 1) {
       this.setState({
         current: current + 1
@@ -34,25 +38,15 @@ class PreviewImages extends React.Component {
     }
   }
 
-  // getImages() {
-  //   console.log(this.props.currentStyle.photos)
-  //   let images;
-  //   // if (this.props.currentStyle.photos.length > 4) {
-  //   //   images = this.props.currentStyle.photos.slice(0, 4);
-  //   // } else {
-  //   //   images = this.props.currentStyle.photos;
-  //   // }
-  //   // this.setState({images});
-  // }
+  handleImageClicked(e) {
+    let images;
+  }
 
-  // componentDidMount() {
-  //   this.getImages();
-  // }
+
 
   render() {
-    const {current, length} = this.state;
-    const {currentStyle, productID} = this.props;
-    // console.log('fuk', currentStyle.photos.length)
+    const { current, length } = this.state;
+    const { currentStyle, productID } = this.props;
 
     let images;
     if (this.props.currentStyle.photos.length > 4) {
@@ -62,38 +56,55 @@ class PreviewImages extends React.Component {
     }
 
     return (
-      <div className="image-carousel">
-        <button className="image-carousel-button  carousel-button-left" onClick={this.prevSlide} >&#10094;</button>
-        {images.map((photo, i) => (
-          <div key={i} className="image-carousel-slide" >
-            {/* <img className="product-image" src={photo.thumbnail_url} alt="Image lost :(" /> */}
-            {i === current && (
-              <img src={photo.thumbnail_url} alt="Image lost :(" className='product-image' />
-            )}
-          </div>
-        ))}
-        <button className="image-carousel-button carousel-button-right" onClick={this.nextSlide} >&#10095;</button>
+      <div className="previewImage-container">
+        <div className="previewImage">
+        <img className='product-image' src={images[0].thumbnail_url} alt="Image lost :(" />
+        </div>
+
+        <div className="image-carousel-row">
+          <button className="image-carousel-button  carousel-button-left" onClick={this.prevSlide} >&#10094;</button>
+          {images.map((photo, i) => (
+            <div key={i} className="image-carousel-slide" >
+                <img className='carousel-image' src={photo.thumbnail_url} alt="Image lost :(" />
+            </div>
+          ))}
+          <button className="image-carousel-button carousel-button-right" onClick={this.nextSlide} >&#10095;</button>
+        </div>
+
       </div>
     )
   }
+
+  // render() {
+  //   const { current, length } = this.state;
+  //   const { currentStyle, productID } = this.props;
+
+  //   let images;
+  //   if (this.props.currentStyle.photos.length > 4) {
+  //     images = this.props.currentStyle.photos.slice(0, 4);
+  //   } else {
+  //     images = this.props.currentStyle.photos;
+  //   }
+
+  //   return (
+  //       <div className="image-carousel">
+  //         <button className="image-carousel-button  carousel-button-left" onClick={this.prevSlide} >&#10094;</button>
+  //         {images.map((photo, i) => (
+  //           <div key={i} className="image-carousel-slide" >
+  //             {/* <img className="product-image" src={photo.thumbnail_url} alt="Image lost :(" /> */}
+  //             {i === current && (
+  //               <img src={photo.thumbnail_url} alt="Image lost :(" className='product-image' />
+  //             )}
+  //           </div>
+  //         ))}
+  //         <button className="image-carousel-button carousel-button-right" onClick={this.nextSlide} >&#10095;</button>
+  //       </div>
+  //   )
+  // }
+
 }
 
 // const PreviewImages = (props) => {
-//   var images;
-//   if (props.currentStyle.photos.length > 4) {
-//     images = props.currentStyle.photos.slice(0, 4);
-//   } else {
-//     images = props.currentStyle.photos;
-//   }
-
-//   nextSlide () {
-//     setCurrent(current === length - 1 ? 0 : current + 1);
-//   };
-
-//   prevSlide () {
-//     setCurrent(current === 0 ? length - 1 : current - 1);
-//   };
-
 //   return (
 //   <div className="image-carousel">
 //     <button className="image-carousel-button  carousel-button-left" onClick={props.onPrevClick}>&#10094;</button>
