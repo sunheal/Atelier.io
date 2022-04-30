0c22ceb987b0d692b1a61c998ee2e52850fbe4b4
sunheal <sunheal@gmail.com>
2022-04-30 16:47:59 -0400

    Updated productId in app

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productID: 64622,
+      productID: 64623,
       productStyle: {},
       relatedProductsIDs: null,
       relatedProductsInfo: [],
